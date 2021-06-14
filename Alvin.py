f15d63bae364a187c0566e5d8fe4a8a79b6f14b3
CharlesElloit <aciforcharles@gmail.com>
2021-06-14 03:46:22 -0700

    moved all skills to a skills folder

--- Alvin.py
+++ Alvin.py
@@ -1,20 +1,24 @@
 from ai import AI
-import pyjokes
+from skills.Joke import joke
+from skills.getDate import getDate
 
 alvin = AI()
-
-def joke():
-  funnyJoke = pyjokes.get_joke()
-  print(funnyJoke)
-  alvin.speak(funnyJoke)
-  
+ 
 command = ""
+wakeWord = "alvin"
 
-while True and command is not "goodbye":
+while True and command != "goodbye":
   command = alvin.listen()
   print("command was:", command)
   
-  if command == "tell me a joke":
-    joke()
+  JOKES_STRS = ["tell me a joke", "tell me another joke", "another one"]
+  for phrase in JOKES_STRS:
+    if phrase in command:
+      joke()
+  
+  TIME_STRS = ["what's the time", "what is the time", "the current time please"]
+  for phrase in TIME_STRS:
+    if phrase in command:
+      getDate()
 
 alvin.speak("Goodbye")
